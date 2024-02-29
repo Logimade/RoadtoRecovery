3b90ea4c3f401c614aca2bca3c6de2c4a982798d
dangerousking <tiagoribeiro24@hotmail.com>
2024-02-29 15:14:04 +0000

    added scripts

---
+++ start CLIENT script.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+START "pythonclient.exe" "C:\Users\HCI4H\Documents\GitHub\RoadtoRecovery\VoiceChat\Medic VoiceChat\pythonclient.exe"
+
+START "basicViewerWin.exe" "C:\Users\HCI4H\Documents\GitHub\RoadtoRecovery\VNC Client App\basicViewerWin.exe"
+
+EXIT
---
+++ start SERVER script.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+START "pythonclient.exe" "C:\Users\HCI4H\Documents\GitHub\RoadtoRecovery\VoiceChat\Ambulance VoiceChat\pythonclient.exe"
+
+START "basicServer.exe" "C:\Users\HCI4H\Documents\GitHub\RoadtoRecovery\VNC Server App\basicServer.exe"
+
+EXIT
