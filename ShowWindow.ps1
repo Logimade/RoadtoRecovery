dacffe0ea10b4e80b182a99aec49af63947e70a6
dangerousking <tiagoribeiro24@hotmail.com>
2024-02-29 17:44:28 +0000

    update scripts

---
+++ ShowWindow.ps1
@@ -0,0 +1,26 @@
+param (
+    [string]$ProcessName,
+    [string]$WindowTitle
+)
+
+# Get the process and window handle
+$windowHandle = (Get-Process | Where-Object { $_.MainWindowTitle -eq $WindowTitle -and $_.ProcessName -eq $ProcessName }).MainWindowHandle
+
+if ([string]::IsNullOrEmpty($windowHandle)) {
+    Write-Host "Window not found."
+} else {
+    try {
+        # Show the window
+        $pinvokeCode = @'
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+'@
+        $type = Add-Type -MemberDefinition $pinvokeCode -Name Win32Utils -Namespace Win32Functions -PassThru
+
+	$type::ShowWindowAsync($windowHandle, 6)# 6 = Minimize
+        $type::ShowWindowAsync($windowHandle, 1) # 1 = Show 
+        Write-Host "Window showed successfully."
+    } catch {
+        Write-Host "Error occurred while minimizing window: $_"
+    }
+}
--- start SERVER script.bat
+++ start SERVER script.bat
@@ -1,9 +1,22 @@
 @ECHO OFF
 
-START "%~dp0" "VoiceChat\Ambulance VoiceChat\pythonclient.exe"
+START /min "Ambulance Voice Chat" "%~dp0VoiceChat\Ambulance VoiceChat\pythonclient.exe"
 
-TIMEOUT /T 2 /NOBREAK
+START "VNC Server" "%~dp0VNC Server App\basicServer.exe"
 
-START "%~dp0" "VNC Server App\basicServer.exe"
+START /min "scrcpy" "%~dp0scrcpy-win64-v2.1.1/scrcpy" --crop 1730:974:1934:450 -d --window-title 'AmbulancePOV'
+
+TIMEOUT /T 1 /NOBREAK
+
+setlocal
+
+rem Define the process name and window title
+set "processName=basicServer"
+set "windowTitle=My Window"
+
+rem Execute the PowerShell script to minimize the window
+powershell -ExecutionPolicy Bypass -File "ShowWindow.ps1" -ProcessName "%processName%" -WindowTitle "%windowTitle%"
+
+endlocal
 
 EXIT
